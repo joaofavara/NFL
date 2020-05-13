58741a32e575361c36a38572b48d365df6313fda
unknown <joaopedro_favara@hotmail.com>
2020-05-13 12:51:56 -0300

    add super bowl qb

--- nfl.R
+++ nfl.R
@@ -7,22 +7,30 @@ hallOfFame <- read.csv(file = "hallOfFame.csv", sep= ";")
 hallOfFame$Player <- as.character(hallOfFame$Player)
 
 
-for (i in 1:length(hallOfFame)) {
+for (i in 1:nrow(hallOfFame)) {
   x <- strsplit(as.character(hallOfFame$Player[i]), "\\\\")[[1]]
   hallOfFame$Player[i] <- x[1]
 }
 
 
-x <- c("teste \ teste2")
-typeof(x)
-x <- as.character(hallOfFame$Player[2])
-x
-x<-strsplit(x, "\\\\")[[1]];
-x[1]
-colnames(plays)
-
 #separate in two dataframes, only with qb's
 qb <- draft$position == 'QB'
 qb <- draft[qb,]
+
+#separate in two dataframes, only with qb's
+qbFame <- hallOfFame$Pos == 'QB'
+qbFame <- hallOfFame[qbFame,]
+
+for (i in 1:nrow(qbFame)) {
+  x <- strsplit(as.character(qbFame$Player[i]), "\\\\")[[1]]
+  qbFame$Player[i] <- x[1]
+}
+
+colnames(qbFame)[2] <- "nameFull" 
+
+intersect(qbFame$nameFull, qb$nameFull)
+intersect(qbFame, qb)
+
+total <- merge(qbFame, qb, by="nameFull")
 
 hist(qb$round)
