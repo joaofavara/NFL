21aa725811cfdd5d1b52ad03ea926b787132b9ec
unknown <joaopedro_favara@hotmail.com>
2020-05-15 08:40:48 -0300

    make the dataframe to change teams name

--- nfl.R
+++ nfl.R
@@ -77,45 +77,51 @@ hist(qb$round)
 
 
 ####### Teams Name #####
-SD = San Diego Chargers
-SL = Los Angeles Rams
-LA = Los Angeles Rams
-BLT = Baltimore Ravens
-LAR = Los Angeles Rams
-HST = Houston Texans
-ARI = Arizona Cardinals
-LAC = Los Angeles Chargers
-BAL = Baltimore Ravens
-ARZ = Arizona Cardinals
-MIN = Minnesota Vikings
-DAL = Dallas Cowboys
-BUF = Buffalo Bills
-NYG = New York Giants
-PIT = Pittsburgh Steelers
-CHI = Chicago Bears
-CIN = Cincinnati Bengals
-TB = Tampa Bay Buccaneers
-GB = Green Bay Packers
-NYJ = New York Jets
-DET = Detroit Lions
-SEA = Seattle Seahawks
-KC = Kansas City Chiefs
-PHI = Philadelphia Eagles
-NE = New England Patriots
-MIA = Miami Dolphins
-CLV = Cleveland Browns
-HO = Tennessee Titans
-NO = New Orleans Saints
-WAS = Washington Redskins
-SF = San Francisco 49ers
-DEN = Denver Broncos
-ATL = Atlanta Falcons
-OAK = Oakland Raiders
-IND = Indianapolis Colts
-ARZ = Arizona Cardinals
-CAR = Carolina Panthers
-JAX = Jacksonville Jaguars
-TEN = Tennessee Titans
+#SD = San Diego Chargers
+#SL = Los Angeles Rams
+#LA = Los Angeles Rams
+#BLT = Baltimore Ravens
+#LAR = Los Angeles Rams
+#HST = Houston Texans
+#ARI = Arizona Cardinals
+#LAC = Los Angeles Chargers
+#BAL = Baltimore Ravens
+#ARZ = Arizona Cardinals
+#MIN = Minnesota Vikings
+#DAL = Dallas Cowboys
+#BUF = Buffalo Bills
+#NYG = New York Giants
+#PIT = Pittsburgh Steelers
+#CHI = Chicago Bears
+#CIN = Cincinnati Bengals
+#TB = Tampa Bay Buccaneers
+#GB = Green Bay Packers
+#NYJ = New York Jets
+#DET = Detroit Lions
+#SEA = Seattle Seahawks
+#KC = Kansas City Chiefs
+#PHI = Philadelphia Eagles
+#NE = New England Patriots
+#MIA = Miami Dolphins
+#CLV = Cleveland Browns
+#HO = Tennessee Titans
+#NO = New Orleans Saints
+#WAS = Washington Redskins
+#SF = San Francisco 49ers
+#DEN = Denver Broncos
+#ATL = Atlanta Falcons
+#OAK = Oakland Raiders
+#IND = Indianapolis Colts
+#ARZ = Arizona Cardinals
+#CAR = Carolina Panthers
+#JAX = Jacksonville Jaguars
+#TEN = Tennessee Titans
+
+abreviation <- c("SD","SL","LA","BLT","LAR","HST","ARI","LAC","BAL","ARZ","MIN","DAL","BUF","NYG","PIT","CHI","CIN","TB","GB","NYJ","DET","SEA","KC","PHI","NE","MIA","CLV","HO","NO","WAS","SF","DEN","ATL","OAK","IND","ARZ","CAR","JAX","TEN")
 
 
+fullNameTeam <- c("San Diego Chargers","Los Angeles Rams","Los Angeles Rams","Baltimore Ravens","Los Angeles Rams","Houston Texans","Arizona Cardinals","Los Angeles Chargers","Baltimore Ravens","Arizona Cardinals","Minnesota Vikings","Dallas Cowboys","Buffalo Bills","New York Giants","Pittsburgh Steelers","Chicago Bears","Cincinnati Bengals","Tampa Bay Buccaneers","Green Bay Packers","New York Jets","Detroit Lions","Seattle Seahawks","Kansas City Chiefs","Philadelphia Eagles","New England Patriots","Miami Dolphins","Cleveland Browns","Tennessee Titans","New Orleans Saints","Washington Redskins","San Francisco 49ers","Denver Broncos","Atlanta Falcons","Oakland Raiders","Indianapolis Colts","Arizona Cardinals","Carolina Panthers","Jacksonville Jaguars","Tennessee Titans")
 
+teamName <- (data.frame(matrix(ncol = length(abreviation), nrow = 0)))
+teamName <- rbind(name, fullNameTeam);
+colnames(teamName) <- abreviation
