496ee84c7e90af8e8165e81700de397627e2963a
unknown <joaopedro_favara@hotmail.com>
2020-05-16 09:54:48 -0300

    get the qbs data related with super bowl

--- nfl.R
+++ nfl.R
@@ -1,5 +1,13 @@
 library(dplyr)
-
+createNameTeamTable <- function() {
+  abreviation <- c("SD","SL","LA","BLT","LAR","HST","ARI","LAC","BAL","ARZ","MIN","DAL","BUF","NYG","PIT","CHI","CIN","TB","GB","NYJ","DET","SEA","KC","PHI","NE","MIA","CLV","HO","NO","WAS","SF","DEN","ATL","OAK","IND","ARZ","CAR","JAX","TEN")
+  
+  fullNameTeam <- c("San Diego Chargers","Los Angeles Rams","Los Angeles Rams","Baltimore Ravens","Los Angeles Rams","Houston Texans","Arizona Cardinals","Los Angeles Chargers","Baltimore Ravens","Arizona Cardinals","Minnesota Vikings","Dallas Cowboys","Buffalo Bills","New York Giants","Pittsburgh Steelers","Chicago Bears","Cincinnati Bengals","Tampa Bay Buccaneers","Green Bay Packers","New York Jets","Detroit Lions","Seattle Seahawks","Kansas City Chiefs","Philadelphia Eagles","New England Patriots","Miami Dolphins","Cleveland Browns","Tennessee Titans","New Orleans Saints","Washington Redskins","San Francisco 49ers","Denver Broncos","Atlanta Falcons","Oakland Raiders","Indianapolis Colts","Arizona Cardinals","Carolina Panthers","Jacksonville Jaguars","Tennessee Titans")
+  
+  teamName <- (data.frame(matrix(ncol = length(abreviation), nrow = 0)))
+  teamName <- rbind(teamName, fullNameTeam);
+  colnames(teamName) <- abreviation
+}
 
 draft <- read.csv(file = "draft.csv", sep= ",")
 gameParticipation <- read.csv(file = "gameParticipation.csv", sep= ",")
@@ -9,7 +17,7 @@ plays <- read.csv(file = "plays.csv", sep= ",")
 hallOfFame <- read.csv(file = "hallOfFame.csv", sep= ";")
 superBowl <- read.csv(file = "Super_Bowl.csv", sep= ",")
 
-
+################################ QBs ####################################
 #separate in two dataframes, only with qb's
 qb <- draft$position == 'QB'
 qb <- draft[qb,]
@@ -17,6 +25,7 @@ qb <- draft[qb,]
 qbUnique <- (unique(qb["draftTeam"]))
 qbUnique
 
+################################ HALL OF FAME ####################################
 #separate in two dataframes, only with qb's
 hallOfFame$Player <- as.character(hallOfFame$Player)
 
@@ -38,45 +47,53 @@ total <- merge(qbFame, qb, by="nameFull")
 total <- unique(total["nameFull"])
 length(total)
 
+############################# QBs - Super Bowl #################################
 #Create the dataframe to make a relactioship between draft qb and superbolw qbs
 superBowl <- read.csv(file = "Super_Bowl.csv", sep= ",")
 
-
+#get the super bolw year
 for (i in 1:nrow(superBowl)) {
   aux <- strsplit(as.character(superBowl$Date[i]), "-")[[1]]
   superBowl$Year[i] <- as.integer(aux[3])
 }
 
-
+#put the column year to second columns in the dataframe
 superBowl <- superBowl[, c(1, 24, 2:23)]
 
-
+#a filter to get the super bolws between 77 <> 18 
 superBowlYear <- (superBowl$Year >= 77 | superBowl$Year <= 18)
 superBowl <- superBowl[superBowlYear,]
 
-
+#change the column name to make the merge in 2 dataframe
 intersect(qb$nameFull, superBowl$QB..Winner)
 colnames(superBowl)[5] <- "nameFull" 
 
-
+#merge the 2 dtaframes
 total <- merge(superBowl, qb, by="nameFull")
-
 total <- total[, c(1, 26:28, 30, 2:4, 7)]
 
-total$nameFull <- as.character(total$nameFull)
+total$draftTeam <- as.character(total$draftTeam)
 
-count(total, vars="nameFull")
+#change the team abreviation name to full name
+createNameTeamTable()
+for (i in 1:nrow(total)) {
+  total$draftTeam[i] <- as.character(teamName[total$draftTeam[i]][[1]])
+}
 
-total
+#get the qbs that win the super bowl from the team that selected he from draft
+indice <- which(total$draftTeam == total$Winner)
 
-table(unique(total["nameFull"]))
-total$Winner
+totalDrfatedBySameTeam <- total[indice,]
+totalDrfatedByDifferentTeam <- total[-indice,]
 
-hist(qb$round)
+#get the qbs that win the super bowl with 1 round pick
+indice <- which(total$round == 1)
+
+totalDrfatedOnFirstRound <- total[indice,]
+totalNotDrfatedOnFirstRound <- total[-indice,]
 
 
-
-####### Teams Name #####
+########################################## Teams Name #############################
 #SD = San Diego Chargers
 #SL = Los Angeles Rams
 #LA = Los Angeles Rams
@@ -119,9 +136,8 @@ hist(qb$round)
 
 abreviation <- c("SD","SL","LA","BLT","LAR","HST","ARI","LAC","BAL","ARZ","MIN","DAL","BUF","NYG","PIT","CHI","CIN","TB","GB","NYJ","DET","SEA","KC","PHI","NE","MIA","CLV","HO","NO","WAS","SF","DEN","ATL","OAK","IND","ARZ","CAR","JAX","TEN")
 
-
 fullNameTeam <- c("San Diego Chargers","Los Angeles Rams","Los Angeles Rams","Baltimore Ravens","Los Angeles Rams","Houston Texans","Arizona Cardinals","Los Angeles Chargers","Baltimore Ravens","Arizona Cardinals","Minnesota Vikings","Dallas Cowboys","Buffalo Bills","New York Giants","Pittsburgh Steelers","Chicago Bears","Cincinnati Bengals","Tampa Bay Buccaneers","Green Bay Packers","New York Jets","Detroit Lions","Seattle Seahawks","Kansas City Chiefs","Philadelphia Eagles","New England Patriots","Miami Dolphins","Cleveland Browns","Tennessee Titans","New Orleans Saints","Washington Redskins","San Francisco 49ers","Denver Broncos","Atlanta Falcons","Oakland Raiders","Indianapolis Colts","Arizona Cardinals","Carolina Panthers","Jacksonville Jaguars","Tennessee Titans")
 
 teamName <- (data.frame(matrix(ncol = length(abreviation), nrow = 0)))
-teamName <- rbind(name, fullNameTeam);
+teamName <- rbind(teamName, fullNameTeam);
 colnames(teamName) <- abreviation
